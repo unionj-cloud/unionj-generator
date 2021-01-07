292cba003f616659ea882a05eac8561c0550856a
qingchang.tang <qingchang96@sina.com>
2021-01-07 20:33:05 +0800

    Constants

--- unionj-generator-backend/src/main/java/cloud/unionj/generator/backend/springboot/Constants.java
+++ unionj-generator-backend/src/main/java/cloud/unionj/generator/backend/springboot/Constants.java
@@ -1,14 +1,16 @@
 package cloud.unionj.generator.backend.springboot;
 
+import java.io.File;
+
 /**
  * @author created by wubin
  * @version v0.1
- *   cloud.unionj.generator.backend.springboot
- *  date 2020/12/22
+ * cloud.unionj.generator.backend.springboot
+ * date 2020/12/22
  */
 public class Constants {
 
-  public static final String OUTPUT_DIR = "backend/springboot";
+  public static final String OUTPUT_DIR = "backend" + File.separator + "springboot";
   public static final String PACKAGE_NAME = "cloud.unionj.demo";
 
 }
