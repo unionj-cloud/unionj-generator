71c2e16cbe6bff314a132c511fbc325ad498016e
qingchang.tang <qingchang96@sina.com>
2021-01-07 21:07:09 +0800

    update

--- unionj-generator-backend/src/main/java/cloud/unionj/generator/backend/springboot/SpringbootFolderGenerator.java
+++ unionj-generator-backend/src/main/java/cloud/unionj/generator/backend/springboot/SpringbootFolderGenerator.java
@@ -21,11 +21,23 @@ public class SpringbootFolderGenerator {
   private boolean zip;
   protected String packageName;
 
+  private String protoPackageName;
+  private String voPackageName;
+
+  private String protoOutputDir;
+  private String voOutputDir;
+
   public static final class Builder {
     private Backend backend;
     private String outputDir = Constants.OUTPUT_DIR;
     private boolean zip = false;
     private String packageName = Constants.PACKAGE_NAME;
+
+    private String protoPackageName;
+    private String voPackageName;
+
+    private String protoOutputDir;
+    private String voOutputDir;
 
     public Builder(Backend backend) {
       this.backend = backend;
@@ -46,12 +58,36 @@ public class SpringbootFolderGenerator {
       return this;
     }
 
+    public Builder protoPackageName(String protoPackageName) {
+      this.protoPackageName = protoPackageName;
+      return this;
+    }
+
+    public Builder voPackageName(String voPackageName) {
+      this.voPackageName = voPackageName;
+      return this;
+    }
+
+    public Builder protoOutputDir(String protoOutputDir) {
+      this.protoOutputDir = protoOutputDir;
+      return this;
+    }
+
+    public Builder voOutputDir(String voOutputDir) {
+      this.voOutputDir = voOutputDir;
+      return this;
+    }
+
     public SpringbootFolderGenerator build() {
       SpringbootFolderGenerator backendFolderGenerator = new SpringbootFolderGenerator();
       backendFolderGenerator.backend = this.backend;
       backendFolderGenerator.outputDir = this.outputDir;
       backendFolderGenerator.zip = this.zip;
       backendFolderGenerator.packageName = this.packageName;
+      backendFolderGenerator.protoPackageName = this.protoPackageName;
+      backendFolderGenerator.voPackageName = this.voPackageName;
+      backendFolderGenerator.protoOutputDir = this.protoOutputDir;
+      backendFolderGenerator.voOutputDir = this.voOutputDir;
       return backendFolderGenerator;
     }
 
