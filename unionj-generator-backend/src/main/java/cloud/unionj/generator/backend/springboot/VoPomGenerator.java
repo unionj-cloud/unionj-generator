6a7a5202e87372df38613249e559bf34a061b2e8
qingchang.tang <qingchang96@sina.com>
2021-01-14 18:43:47 +0800

    VoPomGenerator TODO

---
+++ unionj-generator-backend/src/main/java/cloud/unionj/generator/backend/springboot/VoPomGenerator.java
@@ -0,0 +1,45 @@
+package cloud.unionj.generator.backend.springboot;
+
+import cloud.unionj.generator.DefaultGenerator;
+
+import java.util.Map;
+
+/**
+ * @author created by tqccc
+ * @version v0.0.1
+ * description: cloud.unionj.generator.backend.springboot
+ * date:2021/1/12
+ */
+public class VoPomGenerator extends DefaultGenerator {
+
+  // TODO
+
+  private String outputDir;
+
+  private String groupId;
+
+  private String artifactId;
+
+  private String version;
+
+  private String parentGroupId;
+
+  private String parentArtifactId;
+
+  private String parentVersion;
+
+  @Override
+  public Map<String, Object> getInput() {
+    return null;
+  }
+
+  @Override
+  public String getTemplate() {
+    return null;
+  }
+
+  @Override
+  public String getOutputFile() {
+    return null;
+  }
+}
