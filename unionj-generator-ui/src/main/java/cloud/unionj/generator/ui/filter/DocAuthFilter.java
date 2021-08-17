fbdb64e4c073505ae3a5b7d89ffe7392594cd6b5
JohnnyTing <JohnnyTing2DingXu@gmail.com>
2021-08-17 17:55:45 +0800

    feat: update

--- unionj-generator-ui/src/main/java/cloud/unionj/generator/ui/filter/DocAuthFilter.java
+++ unionj-generator-ui/src/main/java/cloud/unionj/generator/ui/filter/DocAuthFilter.java
@@ -1,6 +1,7 @@
 package cloud.unionj.generator.ui.filter;
 
 import cloud.unionj.generator.ui.config.DocAuthProperties;
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -18,16 +19,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-@WebFilter(filterName = "AuthFilter", urlPatterns = "/**")
+@WebFilter(filterName = "DocAuthFilter", urlPatterns = "/**")
 @RequiredArgsConstructor
 @Slf4j
-public class AuthFilter implements Filter {
+public class DocAuthFilter implements Filter {
   private final DocAuthProperties docAuthProperties;
   private static final String AUTHORIZATION_HEADER = "Authorization";
   private static final String BASIC = "Basic";
   private static final String SPLIT = ":";
   private static final String DOC_PREFIX = "/unionj-cloud";
-  private static final String DefaultUserAndPassword = "admin";
+  private static final String DEFAULT_USER_PASSWORD = "admin";
 
   @Value("${spring.mvc.servlet.path}")
   private String prefix;
@@ -37,26 +38,25 @@ public class AuthFilter implements Filter {
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
     HttpServletRequest httpServletRequest = (HttpServletRequest) request;
     String token = httpServletRequest.getHeader(AUTHORIZATION_HEADER);
-    System.out.printf("prefix: %s\n", prefix);
-    System.out.printf("uri: %s\n", httpServletRequest.getRequestURI());
     if(httpServletRequest.getRequestURI().startsWith(prefix + DOC_PREFIX)){
-      System.out.println();
       boolean passed = false;
-      String userNameAndPassword = StringUtils.removeStart(token, BASIC).trim();
-      if(StringUtils.isNotBlank(userNameAndPassword) && userNameAndPassword.contains(SPLIT)){
-        System.out.printf("user password: %s\n", userNameAndPassword);
-        String[] params = userNameAndPassword.split(SPLIT);
-        String username;
-        String password;
-        if(StringUtils.isBlank(docAuthProperties.getUsername()) || StringUtils.isBlank(docAuthProperties.getPassword())){
-          username = DefaultUserAndPassword;
-          password = DefaultUserAndPassword;
-        }else {
-          username = docAuthProperties.getUsername();
-          password = docAuthProperties.getPassword();
-        }
-        if (username.equals(params[0]) && password.equals(params[1])) {
-          passed = true;
+      if(StringUtils.isNotBlank(token)){
+        String userNameAndPassword = StringUtils.removeStart(token, BASIC).trim();
+        if(StringUtils.isNotBlank(userNameAndPassword)){
+          userNameAndPassword = new String(Base64.decode(userNameAndPassword));
+          String[] params = userNameAndPassword.split(SPLIT);
+          String username;
+          String password;
+          if(StringUtils.isBlank(docAuthProperties.getUsername()) || StringUtils.isBlank(docAuthProperties.getPassword())){
+            username = DEFAULT_USER_PASSWORD;
+            password = DEFAULT_USER_PASSWORD;
+          }else {
+            username = docAuthProperties.getUsername();
+            password = docAuthProperties.getPassword();
+          }
+          if (username.equals(params[0]) && password.equals(params[1])) {
+            passed = true;
+          }
         }
       }
       if(!passed){
